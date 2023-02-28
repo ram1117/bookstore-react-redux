bbaab723d442b03fa0552cfa5e62fc1ef50c6852
ram kumar karuppusamy <ramkumark1@outlook.com>
2023-02-28 13:01:10 +0530

    add click action to button in categories page based on review comments

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,11 +1,13 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categorySlice';
 
 const Categories = () => {
+  const dispatch = useDispatch();
   const status = useSelector((state) => state.category.status);
   return (
     <>
+      <button type="button" onClick={() => { dispatch(checkStatus()); }} className="check-status">Check Status</button>
       <h1>{status}</h1>
-      <button type="button" className="check-status">Check Status</button>
     </>
 
   );
