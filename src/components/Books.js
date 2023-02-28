5166d86879d6daccc0fb64cd900cf0fe0e96a848
ram kumar karuppusamy <ramkumark1@outlook.com>
2023-02-28 19:32:59 +0530

    add fetch books from api functionality

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,9 +1,15 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchBooks } from '../redux/books/bookSlice';
 import Book from './Book';
 import BookForm from './BookForm';
 
 const Books = () => {
+  const dispatch = useDispatch();
   const books = useSelector((state) => state.books.list);
+  useEffect(() => {
+    dispatch(fetchBooks());
+  }, [dispatch]);
   return (
     <>
       <div className="books-container">
--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -1,30 +1,50 @@
-import { createSlice } from '@reduxjs/toolkit';
-import booksList from './booksList';
+import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit';
+// import booksList from './booksList';
 
+const URL_FETCH_BOOKS = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/ofWoEWW0IqPZWqLLrRxu/books';
+// const URL_ADD_BOOKS = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/ofWoEWW0IqPZWqLLrRxu/books';
 const initialState = {
-  list: booksList,
-  status: 'idle',
+  list: [],
   error: null,
 };
+
+export const fetchBooks = createAsyncThunk('books/fetchBooks',
+  async () => {
+    const response = await fetch(URL_FETCH_BOOKS);
+    return response.json();
+  });
 const bookSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
     addBook: (state, { payload }) => {
-      const newState = { ...state };
-      newState.list.push({
-        itemId: `item${state.list.length + 1}`,
+      state.list.push({
+        itemId: nanoid(),
         title: payload.title,
         author: payload.author,
         category: payload.category,
       });
-      return newState;
+      return state;
     },
     removeBook: (state, { payload }) => {
       const newState = { ...state };
       newState.list = state.list.filter((item) => item.itemId !== payload);
       return newState;
     },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchBooks.fulfilled, (state, action) => {
+        const newState = { ...state };
+        const booksList = [];
+        Object.keys(action.payload).forEach((id) => {
+          const result = action.payload[id][0];
+          result.itemId = id;
+          booksList.push(result);
+        });
+        newState.list = booksList;
+        return newState;
+      });
   },
 });
 
